636c90ff303e054a23555d6f0747c7800f877948
Derek Xu <derekxu.uhs@gmail.com>
2020-03-22 14:49:42 -0400

    Created files for all sequelize models, created .example.env file to show expected structure of .env, added dotenv dependency

---
+++ server/models/users.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        userId: {
+            type: DataTypes.INTEGER,
+            field: 'userID',
+            primaryKey: true,
+            autoIncrement: true
+        },
+        firstName: {
+            type: DataTypes.STRING,
+            field: 'firstName',
+            allowNull: false
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            field: 'lastName',
+            allowNull: false
+        }
+    });
+
+    return User
+};
